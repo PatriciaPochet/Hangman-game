c201a1e39c26399760d4b20d156d044d845dc4bf
Patricia Pochet <90600542+PatriciaPochet@users.noreply.github.com>
2022-01-23 22:52:53 +0100

    hangman drawings - given at the beginning
    functions to draw the hangman drawing and written by our trainer to save us time during the dojo

---
+++ hangman_drawings.py
@@ -0,0 +1,113 @@
+def f0():
+  print("\n")
+  print("\n")
+  print("\n")
+  print("\n")
+  print("\n")
+  print("\n")
+  print("\n")
+
+def f1():
+  print("\n")
+  print("\n")
+  print("\n")
+  print("\n")
+  print("\n")
+  print("_____________\n")
+
+def f2():
+  print("\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print("_|____________\n")
+
+def f3():
+  print("_____________\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print("_|____________\n")
+
+def f4():
+  print("_____________\n")
+  print(" | /\n")
+  print(" |/\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print("_|____________\n")
+  print()
+  print("Pour l'instant ça passe ... mais tu as déjà utilisé 4 essais. Un peu de concentration !")
+  print()
+
+def f5():
+  print("_____________\n")
+  print(" | /       |\n")
+  print(" |/\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print("_|____________\n")
+  
+
+def f6():
+  print("_____________\n")
+  print(" | /       |\n")
+  print(" |/        O\n")
+  print(" |\n")
+  print(" |\n")
+  print(" |\n")
+  print("_|____________\n")
+  print()
+  print('Attention ! Tes chances de gagner diminuent ! Plus que 3 essais ! Reprends-toi !')
+  print()
+
+def f7():
+  print("_____________\n")
+  print(" | /       |\n")
+  print(" |/        O\n")
+  print(" |         |\n")
+  print(" |\n")
+  print(" |\n")
+  print("_|____________\n")
+  
+
+def f8():
+  print("_____________\n")
+  print(" | /       |\n")
+  print(" |/        O\n")
+  print(" |        -|-\n")
+  print(" |\n")
+  print(" |\n")
+  print("_|____________\n")
+  print()
+  print("Plus qu'un essai ! C'est ta dernière chance, concentre-toi bien !")
+  print()
+
+def f9():
+  print("_____________\n")
+  print(" | /       |\n")
+  print(" |/        O\n")
+  print(" |        -|-\n")
+  print(" |         /\\\n")
+  print(" |\n")
+  print("_|____________\n")
+  
+
+def f10():
+  print("_____________\n")
+  print(" | /       |\n")
+  print(" |/        O\n")
+  print(" |        -|-\n")
+  print(" |         /\\\n")
+  print(" |\n")
+  print("_|____________\n")
+ 
+def dead_func(pseudo):
+  print("Trop tard {} ! Tu es pendu(e) !!!\n".format(pseudo))
+  print('Quel dommage ! Ne reste pas sur cet échec et retente une nouvelle partie !')
